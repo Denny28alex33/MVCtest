f33a9661bfd953bfd442deb6e1e43e6114c2bc14
Denny28alex33 <157225384+Denny28alex33@users.noreply.github.com>
2026-03-26 00:18:06 +0800

    新增類別新增

--- MVCtest/Data/ApplicationDbContext.cs
+++ MVCtest/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MVCtest.Models;
 
 namespace MVCtest.Data;
 
@@ -9,5 +10,19 @@ public class ApplicationDbContext:DbContext
             options) : base(options)
     {
         
+    }
+    public DbSet<Category>Categories { get; set; }
+    public DbSet<denny>Denny { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "tea", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "fruits tea", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "coffee", DisplayOrder = 3 }
+        );
+        modelBuilder.Entity<denny>().HasData(
+            new denny{Id = 1,Name = "denny",age = 25,DisplayOrder = 1}
+            );
+
     }
 }
--- MVCtest/Program.cs
+++ MVCtest/Program.cs
@@ -1,36 +1,37 @@
-using Microsoft.Extensions.Options;
-using MVCtest.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Options;
+    using MVCtest.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(Options =>
-    Options.UseSqlSever(builder.Configuration.GetConnectionString(
-        "DefaultConnection")));
+    // 將原本的 UseSqlServer 改為 UseNpgsql
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    
+    app.UseHttpsRedirection();
+    app.UseRouting();
 
-app.UseHttpsRedirection();
-app.UseRouting();
+    app.UseAuthorization();
 
-app.UseAuthorization();
+    app.MapStaticAssets();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
 
 
-app.Run();
+    app.Run();
