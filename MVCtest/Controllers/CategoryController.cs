6705261f8b6129ca7089efaabf667ed8b1dd919a
Denny28alex33 <157225384+Denny28alex33@users.noreply.github.com>
2026-03-26 02:53:07 +0800

    新增類別那邊的偵錯

--- MVCtest/Controllers/CategoryController.cs
+++ MVCtest/Controllers/CategoryController.cs
@@ -25,6 +25,22 @@ namespace MVCtest.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","類別名稱不能跟顯示順序一樣");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
 
     }
 }
--- MVCtest/Models/Category.cs
+++ MVCtest/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MVCtest.Models;
@@ -8,6 +9,10 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [MaxLength(30)]
+    [DisplayName("類別名稱")]
     public string Name { get; set; }
+    [DisplayName("顯示順序")]
+    [Range(1,100 ,ErrorMessage = "輸入應該要在1-100之間")]
     public int DisplayOrder { get; set; }
 }
